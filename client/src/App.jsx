ca8bfe867fc30c209a4d2aa08abe0641b3b2a9d3
narrativityy <jake_crowley@comcast.net>
2024-04-26 18:02:53 -0700

    added login and navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
+import AuthContext from './AuthContext';
 import Home from './components/Home'
+import Login from './components/Login'
+import Navbar from './components/Navbar';
+
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false)
+
+  useEffect(() => {
+    if (document.cookie.includes('usertoken')) {
+      setLoggedIn(true)
+    }
+  }, [loggedIn])
 
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
+    <AuthContext.Provider value={{ loggedIn, setLoggedIn }}>
+      {loggedIn ?
+      <Routes>
+        <Route path="/" element={<div><Navbar /><Home /></div>} />
+
+        <Route path="/login" element={<Navigate to="/" />} />
+
+      </Routes> : 
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      }
+    </AuthContext.Provider>
   )
 }
 
